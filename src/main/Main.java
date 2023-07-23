4fe32082916dfac1119bebd29cead5ca1f83bb2c
Helium8 <qq6324756@gmail.com>
2023-07-23 12:21:11 +0800

    add some prompting message add one line to close scanner

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        System.out.println("Please input: ");
         Scanner scanner = new Scanner(System.in);
         System.out.println(scanner.nextLine());
+        scanner.close();
     }
 }
